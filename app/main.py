ee36ffc4d5494ce8c21e686f9c4949f8190bc952
AbdullahMuawia <abdullah.muawia2003@gmail.com>
2026-03-24 11:48:06 +0300

    I have modified my files to commit the final changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from app.database import engine, Base
+from app.routes import auth
 
-app = FastAPI() 
+import app.models.user
+import app.models.vehicle
+import app.models.booking
+
+from app.routes import booking
+from app.routes import vehicle
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(booking.router, prefix="/bookings", tags =["bookings"])
+app.include_router(vehicle.router, prefix="/vehicles", tags=["Vehicles"])
 
 @app.get("/")
 def root():
-    return {"message": "Rental Car API is running"}
+    return {"message": "Rental Car API running"}
+
